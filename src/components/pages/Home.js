8871f40565bcf41dc8c2a0ba1daceefd0cb20e11
Jason Chou <JasonChou0105@gmail.com>
2024-04-09 22:48:39 -0400

    finished

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import Banner from "../Banner/Banner";
 
 import React from "react";
 import Cards from "../Cards/Cards";
+import Footer from "../Footer/Footer";
 
 function Home() {
   return (
     <>
       <Banner />
       <Cards />
+      <Footer />
     </>
   );
 }
